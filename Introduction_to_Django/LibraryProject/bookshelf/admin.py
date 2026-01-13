bc961644ef141664345cd4e0b0120ca7e23db3a2
Codelionme7 <mugishalionel02@gmail.com>
2026-01-13 03:22:40 +0300

    Finalize bookshelf model and CRUD documentation

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from .models import Book
 
-# Customizing the Admin interface
-class BookAdmin(admin.ModelAdmin):
-    # Display these fields in the list view
-    list_display = ('title', 'author', 'publication_year')
-    
-    # Add filters for these fields in the right sidebar
-    list_filter = ('author', 'publication_year')
-    
-    # Enable a search bar to search by title or author
-    search_fields = ('title', 'author')
-
-# Register the model with the custom configuration
-admin.site.register(Book, BookAdmin)
+# Register your models here.
