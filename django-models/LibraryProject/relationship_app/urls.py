2639478faaab752ba04a9bdf9012d00be7a5af19
Codelionme7 <mugishalionel02@gmail.com>
2026-01-16 13:57:49 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,17 +5,22 @@ from .views import LibraryDetailView
 from . import views
 
 urlpatterns = [
-    # Task 1: Book and Library Views
+    # Task 1
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-    # Task 2: Authentication Views
+    # Task 2: Auth
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 
-    # Task 3: Role-Based Views
+    # Task 3: Roles
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
+
+    # Task 4: Permissions (Add these lines)
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
