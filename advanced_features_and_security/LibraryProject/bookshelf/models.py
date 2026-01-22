ac77a96fdd1f2374aed32a73a5fd293a63d11900
Codelionme7 <mugishalionel02@gmail.com>
2026-01-23 01:55:42 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -30,3 +30,25 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# ... (Keep your existing CustomUser and CustomUserManager here) ...
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
