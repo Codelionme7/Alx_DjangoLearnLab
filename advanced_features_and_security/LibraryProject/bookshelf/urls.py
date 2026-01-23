e8f731c2871e16449a587c138495dbd383441595
Codelionme7 <mugishalionel02@gmail.com>
2026-01-23 10:54:55 +0300

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('', include('bookshelf.urls')),
     path('books/', views.book_list, name='book_list'),
     path('books/create/', views.create_book, name='create_book'),
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
