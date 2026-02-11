c66224f67ceba95a6d1991614ee2e67512f52a61
Codelionme7 <mugishalionel02@gmail.com>
2026-02-11 15:29:31 +0300

    Implement user registration and profile management views
    Added user registration and profile update views with authentication.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,2 +1,29 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, UserUpdateForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
