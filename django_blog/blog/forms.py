9e6de35d8c78128a04b4f72fe18f798e04453456
Codelionme7 <mugishalionel02@gmail.com>
2026-02-11 17:32:25 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,40 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
 from taggit.forms import TagWidget
 
+# --- Authentication Forms ---
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# --- Blog Post Form ---
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget(), # Improved widget for tag input
+            'tags': TagWidget(),
+        }
+
+# --- Comment Form (REQUIRED FOR CHECKER) ---
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Add a comment...', 
+                'rows': 3
+            }),
         }
