46b05c515a7a54d964d6dea7cde8e616be847011
Codelionme7 <mugishalionel02@gmail.com>
2026-02-11 16:10:52 +0300

    Enhance Comment model with additional fields
    Added title, published_date, author, and tags fields to Comment model.

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager # Import this
 
 class Post(models.Model):
-    # ... (Keep existing fields) ...
-    # Add related_name='posts' if not already there, though not strictly necessary for this step
-    pass 
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager() # Add this field
 
     def __str__(self):
-        return f'Comment by {self.author} on {self.post}'
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
