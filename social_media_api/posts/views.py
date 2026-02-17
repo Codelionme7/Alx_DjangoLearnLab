d3a3aebb0d40e52dcf6deb97a00086cb17ab0387
Codelionme7 <mugishalionel02@gmail.com>
2026-02-17 15:05:49 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,9 +3,16 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of an object to edit it.
+    """
     def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        # Write permissions are only allowed to the author of the post.
         return obj.author == request.user
 
 class PostViewSet(viewsets.ModelViewSet):
