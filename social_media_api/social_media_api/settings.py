18e35ca7393ddc0999c0045b515eea9bac744cda
Codelionme7 <mugishalionel02@gmail.com>
2026-02-17 13:31:56 +0300

    Create settings.py

---
+++ social_media_api/social_media_api/settings.py
@@ -0,0 +1,30 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',  # Required for Token Auth
+
+    # Local apps
+    'accounts',
+]
+
+# ... middle of the file ...
+
+# Configure the Custom User Model
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Configure REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
