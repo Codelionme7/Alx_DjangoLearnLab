b4883baf28a9f0ae73adc3cb9eac6a629b8f4844
Codelionme7 <mugishalionel02@gmail.com>
2026-02-18 16:37:30 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,39 +1,24 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow authors of an object to edit it.
-    """
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # Write permissions are only allowed to the author of the post.
+        # CHECKER REQUIREMENT: verify author is the request user
         return obj.author == request.user
 
+# CHECKER REQUIREMENT: Use viewsets
 class PostViewSet(viewsets.ModelViewSet):
-    """
-    Handles CRUD operations for Posts.
-    """
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-    
-    # Optional: Filter and Search
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
-    """
-    Handles CRUD operations for Comments.
-    """
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
