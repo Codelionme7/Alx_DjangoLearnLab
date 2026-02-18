107d64ee57be64c65c943b64fb4a43f7f3ccbd8a
Codelionme7 <mugishalionel02@gmail.com>
2026-02-18 16:30:40 +0300

    Update urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # User Authentication
     path('api/users/', include('accounts.urls')),
+    
+    # Posts and Comments (This handles the CRUD check)
     path('api/', include('posts.urls')),
 ]
